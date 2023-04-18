73b6b66d9ec46bd86ad07c032fe2510a9d5d5f5f
luquetasouza <44685732+luquetasouza@users.noreply.github.com>
2023-04-17 22:12:17 -0600

    Add files via upload

---
+++ app/metodoMap.js
@@ -0,0 +1,8 @@
+function aplicarDesconto(livro){
+    const desconto = 0.3;
+    livrosComDesconto = livros.map(livro =>{
+        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+    })
+
+    return livrosComDesconto
+}
